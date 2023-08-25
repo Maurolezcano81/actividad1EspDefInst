ead58dd8b9e080d125d65b3c550b666926e32d50
Maurolezcano81 <maurolezcano111@gmail.com>
2023-08-25 18:06:53 -0300

    Add mainBg and more Css

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ fetch('./libros.json')
 .then(response => response.json())
 .then(results => {
     const elementoMain = document.getElementById('main');
-
-    for (libro in results){
+    const elementoMainOpacity = document.createElement('div');
+    elementoMainOpacity.setAttribute('class', 'main-opacity');
+        for (libro in results){
     //DIV CONTENEDOR
     const elementoDivCard = document.createElement('div');
         elementoDivCard.setAttribute('class', 'card');
@@ -21,17 +22,27 @@ fetch('./libros.json')
         elementoTitulo.textContent = results[libro].nombre;
 
     const elementoAutor = document.createElement('p');
-    elementoAutor.setAttribute('class', 'card-autor');
-    elementoAutor.textContent = "Autor: " + results[libro].autor;
+        elementoAutor.setAttribute('class', 'card-autor');
+        elementoAutor.textContent = "Autor: " + results[libro].autor;
+
+    // DIV BOTON
+    const elementoDivBoton = document.createElement('div');
+    const elementoBoton = document.createElement('button');
+        elementoBoton.setAttribute('class', 'card-boton');
+        elementoBoton.textContent = "Ver mas...";
 
     // APPENDS
     elementoDivTexto.appendChild(elementoTitulo);
     elementoDivTexto.appendChild(elementoAutor);
     elementoDivImg.appendChild(elementoImg);
+    elementoDivBoton.appendChild(elementoBoton);
     elementoDivCard.appendChild(elementoDivImg);
     elementoDivCard.appendChild(elementoDivTexto);
+    elementoDivCard.appendChild(elementoDivBoton)
 
+    
 
-    elementoMain.appendChild(elementoDivCard);
+    elementoMainOpacity.appendChild(elementoDivCard);
+    elementoMain.appendChild(elementoMainOpacity);
     }
 })
